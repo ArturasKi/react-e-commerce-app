d7cd71245ce958efdea58179af61e0c03c784459
Arturito <arturaskisielius91@gmail.com>
2022-08-12 12:47:35 +0300

    Initial project commit, navbar layout

---
+++ myapp/src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { GrSearch, GrCart } from 'react-icons/gr';
+
+function Navbar() {
+  return (
+    <div className="nav-container">
+      <div className="nav-row">
+        <div className="nav-col-1">
+            <div className="search-container">
+                <input></input>
+                <GrSearch />
+            </div>
+        </div>
+        <div className="nav-col-2">
+            <h1>LOGO.</h1>
+        </div>
+        <div className="nav-col-3">
+            <div className="menu-item">REGISTER</div>
+            <div className="menu-item">SIGN IN</div>
+            <div className="menu-item">
+                <GrCart />
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
