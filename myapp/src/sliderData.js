7401ff10d7edf688206f69791b2476dc0da44a72
Arturito <arturaskisielius91@gmail.com>
2022-08-24 14:32:50 +0300

    static images

--- myapp/src/sliderData.js
+++ myapp/src/sliderData.js
@@ -1,21 +1,25 @@
+import summer from './Images/summer.png';
+
+console.log(summer)
+
 const sliderItems = [
     {
         id: 1,
-        img: "https://image.similarpng.com/very-thumbnail/2021/07/Stylish-jeans-pants--on-transparent-background-PNG.png",
+        img: "/static/media/summer.6d3f082e6addc2a5def4.png",
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "#fffaaa"
     },
     {
         id: 2,
-        img: "https://i.pinimg.com/736x/1a/78/db/1a78db456813a41931d1ade736e1c535.jpg",
+        img: "/static/media/autumn.48f24e52b7b461e4f5a9.png",
         title: "AUTUMN SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "#edaeda"
     },
     {
         id: 3,
-        img: "https://www.pinpng.com/pngs/m/405-4059755_leather-winter-coat-png-image-transparent-winter-leather.png",
+        img: "/static/media/winter.cbdd2681408ea9ea9eac.png",
         title: "WINTER SALE",
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
         bg: "#adeeda"
