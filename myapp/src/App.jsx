c8b444806c38de43fd0caadf444b1bd2868a0afe
Arturito <arturaskisielius91@gmail.com>
2022-09-02 13:54:59 +0300

    Routes and navigation added

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Product from './Pages/Product';
 import Home from './Pages/Home';
 import ProductList from './Pages/ProductList';
 
 function App() {
-  return <Product />;
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home show="home" />} />
+          <Route path="/productlist/mens" element={<ProductList show="productlist" />} />
+          <Route path="/productlist/mens/product" element={<Product show="product" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -1,25 +1,51 @@
 import React from "react";
 import { GrSearch, GrCart } from 'react-icons/gr';
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="nav-container">
       <div className="nav-row">
         <div className="nav-col-1">
+            <h1>LOGO.</h1>
+        </div>
+        <div className="nav-col-2">
             <div className="search-container">
                 <input></input>
                 <GrSearch className="icon-search" />
             </div>
         </div>
-        <div className="nav-col-2">
-            <h1>LOGO.</h1>
-        </div>
         <div className="nav-col-3">
-            <div className="menu-item">REGISTER</div>
-            <div className="menu-item">SIGN IN</div>
-            <div className="menu-item">
-                <GrCart />
-            </div>
+            <NavLink to="/productlist/mens" className="menu-item" style={({ isActive }) => // funkcija gauna argumentą isActive, jei true vienas stilus, false - kitas;
+                        isActive ? {
+                            color: 'crimson',
+                            fontWeight: 'bold'
+                        } : undefined
+                        }>MENS</NavLink>
+            <NavLink to="/productlist/womens" className="menu-item" style={({ isActive }) => // funkcija gauna argumentą isActive, jei true vienas stilus, false - kitas;
+                        isActive ? {
+                            color: 'crimson',
+                            fontWeight: 'bold'
+                        } : undefined
+                        }>WOMENS</NavLink>
+            <NavLink to="/register" className="menu-item" style={({ isActive }) => // funkcija gauna argumentą isActive, jei true vienas stilus, false - kitas;
+                        isActive ? {
+                            color: 'crimson',
+                            fontWeight: 'bold'
+                        } : undefined
+                        }>REGISTER</NavLink>
+            <NavLink to="/register" className="menu-item" style={({ isActive }) => // funkcija gauna argumentą isActive, jei true vienas stilus, false - kitas;
+                        isActive ? {
+                            color: 'crimson',
+                            fontWeight: 'bold'
+                        } : undefined
+                        }>SIGN IN</NavLink>
+            <NavLink to="/cart" className="menu-item" style={({ isActive }) => // funkcija gauna argumentą isActive, jei true vienas stilus, false - kitas;
+                        isActive ? {
+                            color: 'crimson',
+                            fontWeight: 'bold'
+                        } : undefined
+                        }><GrCart /></NavLink>
         </div>
       </div>
     </div>
