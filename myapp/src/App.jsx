3c880e48d166b779b081850d31df2a736a65c1ba
Arturito <arturaskisielius91@gmail.com>
2022-09-05 10:30:15 +0300

    Changing size, color states in selected product

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,16 +1,15 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from './Pages/Product';
 import Home from './Pages/Home';
 import ProductList from './Pages/ProductList';
-import { useState } from 'react';
+import AppContext from './Components/AppContext';
 
 function App() {
 
-  const [selectedProduct, setSelectedProduct] = useState([]);
-
-
   return (
+    <AppContext.Provider value={{
+    }}>
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home show="home" />} />
@@ -20,6 +19,7 @@ function App() {
           <Route path="/productlist/womens/product" element={<Product show="product" />} />
       </Routes>
     </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
--- myapp/src/Components/SelectedProduct.jsx
+++ myapp/src/Components/SelectedProduct.jsx
@@ -1,30 +1,45 @@
+import { useState } from "react"
 import { products } from "../data"
 
 function SelectedProduct() {
+
+    const [color, setColor] = useState('Color');
+    const [size, setSize] = useState('Size');
+
+    const selectColor = (e) => {
+        setColor(e.target.value);
+    }
+
+    const selectSize = (e) => {
+        setSize(e.target.value);
+    }
+
   return (
     <div className="sel-container">
         <img src={products[0].img} alt="#" />
         <div className="about">
             <div>
-                <h2>Classic Moc</h2>
+                <h2 style={{color: color}}>Classic Moc</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, hic nisi, recusandae minima dolores at totam voluptatibus ipsum magni adipisci ipsa soluta placeat vitae consectetur repudiandae tenetur, officiis perferendis nihil.</p>
                 <h4>Price: 299.00 EUR</h4>
             </div>
             <div className="col-size">
                 <h5>Color:</h5>
-                <select>
-                    <option value='0'>Color</option>
-                    <option value='1'>Briar</option>
-                    <option value='2'>Brown</option>
-                    <option value='3'>Black</option>
+                <select onChange={selectColor} value={color}>
+                    <option value='color'>Color</option>
+                    <option value='red'>Red</option>
+                    <option value='brown'>Brown</option>
+                    <option value='green'>Green</option>
                 </select>
                 <h5>Size:</h5>
-                <select>
-                    <option value='0'>Size</option>
-                    <option value='1'>42</option>
-                    <option value='2'>43</option>
-                    <option value='3'>44</option>
+                <select onChange={selectSize} value={size}>
+                    <option value=''>Size</option>
+                    <option value='42'>42</option>
+                    <option value='43'>43</option>
+                    <option value='44'>44</option>
                 </select>
+                <div className="col-size" style={color === 'Color' ? {display: 'none'} : {display: 'block'}}>Selected color: <b>{color[0].toUpperCase() + color.slice(1).toLowerCase()}</b></div>
+                <div className="col-size" style={size === 'Size' ? {display: 'none'} : {display: 'block'}}>Selected size: <b>{size}</b></div>
             </div>
         </div>
     </div>
