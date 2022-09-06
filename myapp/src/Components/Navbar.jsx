3bae4cc95512bb6ddbe776c32b5ad2460af27c47
Arturito <arturaskisielius91@gmail.com>
2022-09-06 18:17:47 +0300

    Search bar and nav update (not finished)

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import { GrSearch, GrCart } from 'react-icons/gr';
+import { RiAccountCircleLine } from 'react-icons/ri';
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
 
   const [navbar, setNavbar] = useState(false);
+  const [searchToggle, setSearchToggle] = useState(false);
 
   const changeBackground = () => {
     if(window.scrollY >= 90) {
@@ -16,6 +19,14 @@ function Navbar() {
 
   window.addEventListener('scroll', changeBackground);
 
+  const click = () => {
+      setSearchToggle(!searchToggle)
+    }
+
+  useEffect(() => {
+    setTimeout(3000);
+  }, [setSearchToggle])
+
   return (
     <div className={navbar ? 'nav-container active' : 'nav-container'}>
       <div className="nav-row">
@@ -23,12 +34,6 @@ function Navbar() {
           <NavLink to="/" className="menu-item"><h1>LOGO.</h1></NavLink>
         </div>
         <div className="nav-col-2">
-            <div className="search-container">
-                <input></input>
-                <GrSearch className="icon-search" />
-            </div>
-        </div>
-        <div className="nav-col-3">
             <NavLink to="/productlist/mens" className="menu-item" style={({ isActive }) => // funkcija gauna argumentą isActive, jei true vienas stilus, false - kitas;
                         isActive ? {
                             color: 'crimson',
@@ -46,20 +51,33 @@ function Navbar() {
                             color: 'crimson',
                             fontWeight: 'bold'
                         } : undefined
-                        }>REGISTER</NavLink>
+                        }>BLOG</NavLink>
             <NavLink to="/register" className="menu-item" style={({ isActive }) => // funkcija gauna argumentą isActive, jei true vienas stilus, false - kitas;
                         isActive ? {
                             color: 'crimson',
                             fontWeight: 'bold'
                         } : undefined
-                        }>SIGN IN</NavLink>
+                        }>SALE</NavLink>
+           
+        </div>
+        <div className="nav-col-3">
+            <button onClick={click}><GrSearch className="icon-search" /></button>
             <NavLink to="/cart" className="menu-item" style={({ isActive }) => // funkcija gauna argumentą isActive, jei true vienas stilus, false - kitas;
                         isActive ? {
                             color: 'crimson',
                             fontWeight: 'bold'
                         } : undefined
                         }><GrCart /></NavLink>
+            <NavLink to="/account" className="menu-item" style={({ isActive }) => // funkcija gauna argumentą isActive, jei true vienas stilus, false - kitas;
+                        isActive ? {
+                            color: 'crimson',
+                            fontWeight: 'bold'
+                        } : undefined
+                        }><RiAccountCircleLine /></NavLink>
         </div>
+      </div>
+      <div className={searchToggle ? "search-open search-close" : "search-open"}>
+        <input></input>
       </div>
     </div>
   );
