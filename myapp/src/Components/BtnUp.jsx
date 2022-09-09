fee0d0ec9d9b57e235dcd19440465b67c2c297cb
Arturito <arturaskisielius91@gmail.com>
2022-09-09 10:38:14 +0300

    Scroll to top btn

--- myapp/src/Components/BtnUp.jsx
+++ myapp/src/Components/BtnUp.jsx
@@ -1,22 +1,28 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { TbArrowBigTop } from "react-icons/tb";
 
 function BtnUp() {
+  const [backToTop, setBackToTop] = useState(false);
 
-  const btnUp = document.querySelector('.btn-up');
-  console.log(btnUp)
-
-  // btnUp.addEventListener('scroll', );
-
-  // function appear() {
-  //   // if (window.screenY >= 200) {
-  //   //   btnUp.classList.add('.btn-up')
-  //   // }
-  // }
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 200) {
+        setBackToTop(true);
+      } else {
+        setBackToTop(false);
+      }
+    });
+  }, []);
 
   return (
     <div className="btn-up">
-      <button onClick={() => window.scrollTo(0, 0)}><TbArrowBigTop /></button>
+      {backToTop && (
+        <button onClick={() => window.scrollTo(0, 0)}>
+          <TbArrowBigTop />
+        </button>
+      )}
     </div>
   );
 }
