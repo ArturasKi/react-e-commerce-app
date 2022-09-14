f502bd9354922b4f63ecc3fdb27cc09b247a47ac
Arturito <arturaskisielius91@gmail.com>
2022-09-14 11:39:28 +0300

    Cart amount update

--- myapp/src/Components/CartProduct.jsx
+++ myapp/src/Components/CartProduct.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 function CartProduct() {
+
+  const [count, setCount] = useState(1);
+
   let cart = JSON.parse(localStorage.getItem("cart_item"));
 
   const removeItem = () => {
     localStorage.removeItem("cart_item");
   };
+
+  const minusCount = () => {
+    if(count > 1) {
+      setCount(count => count - 1);
+    }
+  }
+
+  const plusCount = () => {
+    setCount(count => count + 1);
+  }
 
   return (
     <div className="cart-container">
@@ -11,14 +26,17 @@ function CartProduct() {
         <div className="cart-row">
           <img src={cart ? cart.img : null} alt="." />
           <div className="cart-info">
-            <div>
               <h2>{cart ? cart.title : null}</h2>
-              <h5>Color: {cart ? cart.color : null}</h5>
-              <h5>Size: {cart ? cart.size : null}</h5>
-              <h5>Price: {cart ? cart.price : null}</h5>
+              <p>Color: {cart ? cart.color : null}</p>
+              <p>Size: {cart ? cart.size : null}</p>
+              <p>Price: {cart ? cart.price : null}</p>
+              <div className="amount">
+                <button onClick={minusCount}>-</button>
+                <b style={{padding: '10px'}}>{count}</b>
+                <button onClick={plusCount}>+</button>
+              </div>
               <button onClick={removeItem}>Remove item</button>
             </div>
-          </div>
         </div>
       ) : (
         "Empty cart"
