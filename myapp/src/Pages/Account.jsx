97157098bfdb45cb8a7356d9bc0e99b1abadcdf3
Arturito <arturaskisielius91@gmail.com>
2023-04-02 18:47:04 +0300

    html structure changes

--- myapp/src/Pages/Account.jsx
+++ myapp/src/Pages/Account.jsx
@@ -3,54 +3,66 @@ import "./scss/account.scss";
 
 function Account() {
   const [labelEmail, setLabelEmail] = useState("");
+  const [labelPassword, setLabelPassword] = useState("");
 
-  const inputEmail = document.getElementById("email");
+  const setEmailLabel = (e) => {
+    if (e.target.value !== "") {
+      setLabelEmail("Email");
+    } else setLabelEmail("");
+  };
 
-  const setLabel = () => {
-    if (inputEmail === "") {
-      return null
-    }
-    if (inputEmail !== "") {
-      setLabelEmail('Email')
-    }
-  }
+  const setPasswordLabel = (e) => {
+    if (e.target.value !== "") {
+      setLabelPassword("Password");
+    } else setLabelPassword("");
+  };
 
   return (
     <div className="accountWrapper">
       <div className="accountWrapper__formBlock">
         <div className="accountWrapper__form">
-          <div className="accountWrapper__formRow">
-            <div className="accountWrapper__signUpContent">
-              <div className="accountWrapper__title">Welcome back!</div>
-              <div className="accountWrapper__inputs">
-                <label>{labelEmail}</label>
-                <input onChange={setLabel} id="email" placeholder="Email" />
-                <label>Password</label>
-                <input />
-              </div>
-              <div className="accountWrapper__bottom">
-                <button className="accountWrapper__btn">Login</button>
-              </div>
+          <div className="accountWrapper__formColumn">
+            <div className="accountWrapper__title">Welcome back!</div>
+            <div className="accountWrapper__formRow">
+              <label>{labelEmail}</label>
+              <input
+                type="email"
+                onChange={setEmailLabel}
+                placeholder="Email"
+              />
+            </div>
+            <div className="accountWrapper__formRow">
+              <label>{labelPassword}</label>
+              <input
+                type="password"
+                onChange={setPasswordLabel}
+                placeholder="Password"
+              />
+            </div>
+            <div className="accountWrapper__bottom">
+              <button className="accountWrapper__btn">Login</button>
             </div>
           </div>
-          <div className="accountWrapper__signInForm">
-            <div className="accountWrapper__signInContent">
-              <div className="accountWrapper__title">Hello there!</div>
-              <div className="accountWrapper__inputs">
-                <label>First name</label>
-                <input />
-                <label>Last name</label>
-                <input />
-                <label>Email</label>
-                <input />
-                <label>Password</label>
-                <input />
-              </div>
-              <div className="accountWrapper__bottom">
-                <button className="accountWrapper__btn">
-                  Create my account
-                </button>
-              </div>
+          <div className="accountWrapper__formColumn">
+            <div className="accountWrapper__title">Hello there!</div>
+            <div className="accountWrapper__formRow">
+              <label>First name</label>
+              <input />
+            </div>
+            <div className="accountWrapper__formRow">
+              <label>Last name</label>
+              <input />
+            </div>
+            <div className="accountWrapper__formRow">
+              <label>Email</label>
+              <input />
+            </div>
+            <div className="accountWrapper__formRow">
+              <label>Password</label>
+              <input />
+            </div>
+            <div className="accountWrapper__bottom">
+              <button className="accountWrapper__btn">Create my account</button>
             </div>
           </div>
         </div>
